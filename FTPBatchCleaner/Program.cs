1a95b5e9ac96355ad424120656f47a0dfb6edc81
qianzhongxiang <qianzhongxiang@outlook.com>
2024-05-10 09:16:13 +0800

    add FTPBatchCleaner

---
+++ FTPBatchCleaner/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FluentFTP;
+using FTPBatchCleaner;
+
+Console.WriteLine("Hello, World!");
+
+var client = new FtpClient(Setting.Instance.HostName, Setting.Instance.UserName, Setting.Instance.Pwd);
+client.AutoConnect();
+List<Task> tasks = new List<Task>();
+//foreach (var dir in Setting.Instance.Dirs)
+//{
+//    tasks.Add(Task.Run(() =>
+//    {
+//        var list = client.GetListing(dir).ToDictionary(i => i.Name, i => i);
+//        if (list.Count > 30)
+//        {
+//            var slist = new System.Collections.SortedList(list);
+//            var enumer = slist.GetEnumerator();
+//            for (int i = 0; i < list.Count - 30; i++)
+//            {
+//                enumer.MoveNext();
+//                var item = enumer.Value as FtpListItem;
+//                if (item.Type == FtpObjectType.File)
+//                {
+//                    client.DeleteFile(item.FullName);
+//                }
+//                else if (item.Type == FtpObjectType.Directory)
+//                {
+//                    client.DeleteDirectory(item.FullName);
+//                }
+//                Console.WriteLine($"{enumer.Value} was removed");
+//            }
+//        }
+//    }));
+//}
+
+//Task.WhenAll(tasks.ToArray()).ContinueWith(t =>
+//{
+//    if (t.IsFaulted)
+//    {
+//        foreach (var ex in t.Exception.InnerExceptions)
+//        {
+//            Console.Error.WriteLine(ex.ToString());
+//        }
+//    }
+//});
+
+
+foreach (var dir in Setting.Instance.Dirs)
+{
+    try
+    {
+
+        var list = client.GetListing(dir).ToDictionary(i => i.Name, i => i);
+        if (list.Count > 30)
+        {
+            var slist = new System.Collections.SortedList(list);
+            var enumer = slist.GetEnumerator();
+            for (int i = 0; i < list.Count - 30; i++)
+            {
+                enumer.MoveNext();
+                var item = enumer.Value as FtpListItem;
+                if (item.Type == FtpObjectType.File)
+                {
+                    client.DeleteFile(item.FullName);
+                }
+                else if (item.Type == FtpObjectType.Directory)
+                {
+                    client.DeleteDirectory(item.FullName);
+                }
+                Console.WriteLine($"{enumer.Value} was removed");
+            }
+        }
+
+    }
+    catch (Exception ex)
+    {
+
+        Console.Error.WriteLine(ex.ToString());
+
+    }
+}
---
+++ FTPBatchCleaner/Setting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPBatchCleaner
+{
+    public class Setting
+    {
+        private static Setting _instance;
+        public static Setting Instance => _instance ?? (_instance = Init());
+        private static Setting Init()
+        {
+            var assembly = System.Reflection.Assembly.GetAssembly(typeof(Setting));
+            var dir = System.IO.Path.GetDirectoryName(assembly.Location);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Setting>(System.IO.File.ReadAllText(System.IO.Path.Combine(dir, "setting.json")));
+        }
+        public string UserName { get; set; } = "installpackage";
+        public string Pwd { get; set; } = "cn22Q6Yd";
+        public string HostName { get; set; } = "192.168.10.101";
+        public List<string> Dirs { get; set; }
+    }
+}
